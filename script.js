de001921cc090a27faf6ab6b52c58ea1f55031bd
Amanda <61156616+amanda-low@users.noreply.github.com>
2024-12-27 12:55:29 +0100

    Commit
    Added header template, work page

--- script.js
+++ script.js
@@ -112,3 +112,13 @@ function checkPassword() {
       compare()
     }
   }
+
+  // Header
+
+  document.addEventListener('DOMContentLoaded', function() {
+    fetch('header.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+      });
+  });
